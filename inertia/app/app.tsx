13128a77e6daed62d614d0c267a0988cb5cf0b19
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-03 10:49:10 +0100

    refacto: comment the import resolvePageComponent from inertia

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -4,7 +4,7 @@
 import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+// import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
